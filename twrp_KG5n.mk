5602ae739ab0161a9280fd12fa0a8142839f88af
Arturmes <160504276+Arturmes@users.noreply.github.com>
2024-05-25 07:03:11 +0000

    twrp_KG5n.mk: aosp?

--- twrp_KG5n.mk
+++ twrp_KG5n.mk
@@ -1,16 +1,19 @@
+# Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# INHERIT GSI KEYS
+# Install gsi keys into ramdisk, to boot a developer GSI with verified boot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# INHERIT TWRP
+# Inherit some common TWRP stuff
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # INHERIT FROM THIS DEVICE TREE
