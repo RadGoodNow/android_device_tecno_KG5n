ffedb4350c642581ff0ec1b4fbed0261d9939c73
Arturmes <160504276+Arturmes@users.noreply.github.com>
2024-05-25 07:49:35 +0000

    twrp_KG5n.mk: !aosp

--- twrp_KG5n.mk
+++ twrp_KG5n.mk
@@ -1,6 +1,6 @@
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
